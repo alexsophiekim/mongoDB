0ba13706358770557d05008126bd287a9e0e0ace
Sophie Kim <270010565@bun2-206-17.local>
2019-08-22 11:51:35 +1200

    doing add.post

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const allProducts = require('./data/products');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
@@ -19,36 +25,31 @@ app.get('/allProducts', function(req,res){
 
 app.get('/product/:id', function(req,res){
     const id = req.params.id;
-    let filteredData = [];
     for (var i = 0; i < allProducts.length; i++) {
-      if (allProducts[i].id.toString()===id) {
-          filteredData.push(allProducts[i]);
-          console.log(filteredData[0]);
+      if (allProducts[i].id.toString()==id) {
+          res.send(allProducts[i])
+          break;
       }
     }
-    res.send(filteredData[0])
 });
 
-app.get('/product/name=:name',function(req,res){
-  const name = req.params.name;
-  let filteredData = [];
-  for (var i = 0; i < allProducts.length; i++) {
-    for (var j = 0; j < name.length; i++) {
-      if (name.toString() == inputName) {
-        console.log(name);
-      }
-    }
+app.post('/product',function(req,res){
+  console.log(req.body);
+  let product = {
+      name: req.body.name,
+      price: req.body.price,
+      message: 'We are about to send this product to a database'
   }
-  res.send(filteredData[0]);
+  res.send(product);
 })
 
-app.get('/product/edit:id',function(req,res){
 
-});
 
-app.get('/product/delete/:id',function(req,res){
 
-});
+
+
+
+
 app.listen(port, () => {
     console.clear();
     console.log(`application is running on port ${port}`)
